44b9eb2a25fb5b9b005131d59b4bc338fbc82886
Shubham Mehra <subhammehra4@gmail.com>
2021-05-06 12:16:26 +0530

    Commit

---
+++ utils/index.d.test.ts
@@ -0,0 +1,29 @@
+import { Equal, Expect, NotEqual, IsAny, ExpectFalse } from './index'
+
+type cases = [
+  /* Expect */
+  Expect<true>,
+  // @ts-expect-error
+  Expect<false>,
+
+  /* ExpectFalse */
+  ExpectFalse<false>,
+  // @ts-expect-error
+  ExpectFalse<true>,
+
+  /* Equal */
+  Expect<Equal<true, true>>,
+  ExpectFalse<Equal<false, true>>,
+  Expect<Equal<'123', '123'>>,
+  ExpectFalse<Equal<'123', string>>,
+  Expect<Equal<{ a: number }, { a: number }>>,
+  ExpectFalse<Equal<{ a: number }, { b: number }>>,
+
+  /* Not Equal */
+  Expect<NotEqual<false, true>>,
+  ExpectFalse<NotEqual<true, true>>,
+
+  /* IsAny */
+  Expect<IsAny<any>>,
+  ExpectFalse<IsAny<1>>,
+]
